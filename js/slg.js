96484c55a9e998b1213829ac8bedec6e76683718
maria <maria@gmail.com>
2024-11-14 21:29:37 +0200

    dumb

---
+++ js/slg.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector('.wrapper')
+const question = document.querySelector('.question')
+const yesBtn = document.querySelector('#yes')
+const noBtn = document.querySelector('#no')
+const confirmation = document.querySelector('.confirmation')
+
+const yesAnswerMethod = () => {
+    question.style.display = 'none';
+    confirmation.style.display = 'block';
+
+}
+
+const noAnswerMethod = () => {
+    noBtn.style.position = 'absolute';
+
+    const xPosition = Math.floor(Math.random() * wrapper.clientWidth);
+    const yPosition = Math.floor(Math.random() * wrapper.clientHeight);
+
+    noBtn.style.left = xPosition >= wrapper.clientWidth / 2 ? `${xPosition - noBtn.clientWidth}px` : `${xPosition}`;
+    noBtn.style.top = yPosition >= wrapper.clientHeight / 2 ? `${yPosition - noBtn.clientHeight}px` : `${yPosition}`;
+}
+
+yesBtn.addEventListener('click', yesAnswerMethod);
+
+noBtn.addEventListener('click' , noAnswerMethod);
